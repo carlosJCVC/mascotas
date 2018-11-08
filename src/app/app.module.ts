7fd24f032a8a2ef9d5c84f6162891ce81322a43b
Fernando Soto <39037895+K4waiiChan@users.noreply.github.com>
2018-11-08 16:40:32 -0400

    imports and commits deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,73 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-import { environment } from '../environments/environment';
-import { LayoutModule } from '@angular/cdk/layout';
-import { CommonModule, DatePipe } from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { PetService } from './components/admin/pets/pet.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from "./material.module";
 
-import { InputComponent } from "./components/fields/input/input.component";
-import { ButtonComponent } from "./components/fields/button/button.component";
-import { SelectComponent } from "./components/fields/select/select.component";
-import { DynamicFieldDirective } from "./components/dynamic-field/dynamic-field.directive";
-import { DynamicFormComponent } from "./components/dynamic-form/dynamic-form.component";
 
-//Mascotas
-// import { IndexComponent } from './components/pets/index/index.component';
-// import { CreateComponent } from "./components/pets/create/create.component";
-// import { PetsComponent } from "./components/pets/pets.component";
-// import { MatConfirmDialogComponent } from './components/mat-confirm-dialog/mat-confirm-dialog.component';
 import { CoreModule } from './components/admin/admin.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    // NavBarComponent,
-    // InputComponent,
-    // ButtonComponent,
-    // SelectComponent,
-    // DynamicFieldDirective,
-    // DynamicFormComponent,
-    // CreateComponent,
-    // IndexComponent,
-    // MatConfirmDialogComponent,
-    // PetsComponent,
   ],
   imports: [
     BrowserModule,
-    // MaterialModule,
-    // BrowserModule,
-    //LayoutModule,
     BrowserAnimationsModule,
-    //FormsModule,
-    //ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
-    //CommonModule,
     CoreModule,
   ],
-  providers: [
-      // PetService,
-      // DatePipe
-  ],
+  providers: [],
   bootstrap: [AppComponent],
-  entryComponents: [
-    // CreateComponent,
-    // InputComponent,
-    // ButtonComponent,
-    // SelectComponent,
-    // MatConfirmDialogComponent
-  ]
+  entryComponents: []
 })
 
 export class AppModule { }
