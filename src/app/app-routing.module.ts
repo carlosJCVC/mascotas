29f752133348ebb1917949990a0ab8282a4e913a
Fernando Soto <39037895+K4waiiChan@users.noreply.github.com>
2018-11-08 16:35:30 -0400

    thinks deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-//import { IndexComponent } from './components/pets/index/index.component';
-//import { CreateComponent } from './components/pets/create/create.component';
-import { LoginComponent } from './components/login/login.component';
 import { LoginModule } from './components/login/login.module';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   children: [
-  //     { path: 'lista_mascotas', component: IndexComponent },
-  //     { path: 'registrar_mascota', component: CreateComponent },
-  //   ]
-  // },
     { path: 'auth', loadChildren: './components/auth/auth.module#AuthModule' },
     { path: 'login', loadChildren: './components/login/login.module#LoginModule' },
     { path: '**', redirectTo: 'auth/dashboard' },
